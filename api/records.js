21367c22effaf0561b85e1b441b442afdd74e85f
Apple0224 <Apple0224@users.noreply.github.com>
2026-05-28 16:36:22 +0800

    debug: edge runtime查看环境变量

--- api/records.js
+++ api/records.js
@@ -1,4 +1,5 @@
-// 先用debug模式看看有哪些环境变量
+export const config = { runtime: 'edge' };
+
 export default async function handler(req) {
   const headers = {
     'Access-Control-Allow-Origin': '*',
@@ -9,20 +10,9 @@ export default async function handler(req) {
     return new Response(null, { status: 204, headers });
   }
 
-  // Debug: 返回所有Redis/KV相关的环境变量名
-  const envKeys = Object.keys(process.env).filter(k =>
-    /REDIS|KV|UPSTASH/i.test(k)
-  );
-  const envInfo = {};
-  envKeys.forEach(k => {
-    const v = process.env[k];
-    // 只显示URL的部分内容，隐藏密码
-    envInfo[k] = v && v.length > 50 ? v.substring(0, 30) + '...' + v.substring(v.length - 20) : (v ? '***' : 'empty');
-  });
-
-  return new Response(JSON.stringify({
-    message: "Debug mode - showing Redis/KV env vars",
-    envVars: envInfo,
-    allEnvKeys: Object.keys(process.env).sort()
-  }, null, 2), { headers });
+  // 列出所有环境变量名
+  const allKeys = Object.keys(process.env).sort();
+  const redisKeys = allKeys.filter(k => /REDIS|KV|UPSTASH/i.test(k));
+  
+  return new Response(JSON.stringify({ redisKeys, allKeys }, null, 2), { headers });
 }
